3e6a41347284545447d098ff2706daf4e20d8388
gary909 <25279957+gary909@users.noreply.github.com>
2023-12-09 20:19:49 +0100

    public health int received, player no longer picks up if Health >= 100

--- HealthPickup.cs
+++ HealthPickup.cs
@@ -1,19 +1,17 @@
+using EmeraldAI.Example;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// A script to add to health pickups, once picked up
-/// the object is hidden from view and the amount of 
-/// health is sent to the 'Player'.  The following is added
-/// in the player health script;
-/// 
+///   A script to add to health pickups, once picked up the object is hidden from view and the amount of health is sent to the 'Player'.  
+///   If player health >= 100 the pickup is ignored. The following is added in the player health script;
+///   
 ///    void ApplyHeal(int heal)
 ///    {
 ///    CurrentHealth += heal; // add health points from healthPickup
 ///    UpdateHealth_UI_Text(); // upDate UI Text 
-///    }
-///    
+///    }   
 /// </summary>
 
 public class HealthPickup: MonoBehaviour
@@ -22,14 +20,25 @@ public class HealthPickup: MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.transform.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            //Destroy(gameObject); // Too slow, removed
-            gameObject.GetComponent<MeshRenderer>().enabled = false; // Disable mesh renderer to look like its been picked up
-            gameObject.GetComponent<Collider>().enabled = false; // Disable mesh Collider once it's been picked up
-            other.transform.SendMessage("ApplyHeal", healthAmount);
-            //Debug.Log("Player picked up Health"); // Message to show working
+            GameObject player = other.gameObject; // Find the player GameObject
+            EmeraldAIPlayerHealth healthScript = player.GetComponent<EmeraldAIPlayerHealth>(); // Attempt to get the EmeraldAIPlayerHealth script on the player GameObject
+            int currentHealth = healthScript.CurrentHealth; // Access CurrentHealth value in EmeraldAIPlayerHealth.cs
+            //Debug.Log("Current Health: " + currentHealth);
+            if (currentHealth >= 100) // If health >= 100 do nothing
+            {
+                return;
+            } 
+                else // Pickup health
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = false; // Disable mesh renderer
+                gameObject.GetComponent<Collider>().enabled = false; // Disable collider
+                player.SendMessage("ApplyHeal", healthAmount); // Apply healing to the player
+                // Debug.Log("Player picked up Health"); // Message to show working
+            }
         }
     }
 }
+
 
