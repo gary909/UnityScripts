8c71623b6eeaf51e5c7f410f57b2f72bae596bc2
gary909 <25279957+gary909@users.noreply.github.com>
2023-12-16 18:37:22 +0100

    script being found

--- AmmoPickupPistol.cs
+++ AmmoPickupPistol.cs
@@ -2,42 +2,39 @@ using EmeraldAI.Example;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BNG;
 
-/// <summary>
-///   A script to add to health pickups, once picked up the object is hidden from view and the amount of health is sent to the 'Player'.  
-///   If player health >= 100 the pickup is ignored. The following is added in the player health script;
-///   
-///    void ApplyHeal(int heal)
-///    {
-///    CurrentHealth += heal; // add health points from healthPickup
-///    UpdateHealth_UI_Text(); // upDate UI Text 
-///    }   
+/// <summary> 
 /// </summary>
 
 public class AmmoPickupPistol: MonoBehaviour
 {
-    public int healthAmount = 25;
+    
+    private void Start()
+    {
+        int pistolClips = FindObjectOfType<AmmoDispenser>().CurrentPistolClips;
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
+        int pistolClips = FindObjectOfType<AmmoDispenser>().CurrentPistolClips;
         if (other.CompareTag("Player"))
         {
-            GameObject player = other.gameObject; // Find the player GameObject
-            EmeraldAIPlayerHealth healthScript = player.GetComponent<EmeraldAIPlayerHealth>(); // Attempt to get the EmeraldAIPlayerHealth script on the player GameObject
-            int currentHealth = healthScript.CurrentHealth; // Access CurrentHealth value in EmeraldAIPlayerHealth.cs
-            //Debug.Log("Current Health: " + currentHealth);
-            if (currentHealth >= 100) // If health >= 100 do nothing
+            
+            if (pistolClips >= 10) // If max amount of pistol clips reached, don't pick up, 
             {
                 return;
-            } 
-                else // Pickup health
+            }
+            else // Pickup health
             {
+                Debug.Log( "You have this many pistol clips: " + pistolClips);
                 gameObject.GetComponent<MeshRenderer>().enabled = false; // Disable mesh renderer
                 gameObject.GetComponent<Collider>().enabled = false; // Disable collider
-                player.SendMessage("ApplyHeal", healthAmount); // Apply healing to the player
-                // Debug.Log("Player picked up Health"); // Message to show working
+                //player.SendMessage("ApplyArmourMega", armourAmount); // Apply armour to the player
             }
         }
     }
+
 }
 
